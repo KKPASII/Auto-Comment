b632c0badb1810e7d3961a7c0f6484115a6565ff
KKPASII <kkongpasi1005@gmail.com>
2026-03-19 21:59:54 +0900

    Project Setup

---
+++ src/main/java/com/hamplz/autocomment/AutocommentApplication.java
@@ -0,0 +1,13 @@
+package com.hamplz.autocomment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocommentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocommentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamplz/autocomment/AutocommentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamplz.autocomment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocommentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
